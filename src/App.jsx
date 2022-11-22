2d97e9bbde50159afe2fe2428e8aaab4a97317f1
sahilsalmanov <salmanovsahil7@gmail.com>
2022-11-22 23:34:06 +0400

    end

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+       <p> Now I can create component
+        </p> 
+    )
+  }
+}
+export default App
